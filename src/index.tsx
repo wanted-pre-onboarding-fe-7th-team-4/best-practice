4e7038e05321fa173d564184e8fd96b49ab5137e
A <movie3342@gmail.com>
2022-10-27 19:30:05 +0900

    feat:App, index 세팅

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import App from "./App";
-import { theme } from "./lib/styles";
+import { GlobalStyle, theme } from "./lib/styles";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -12,6 +12,7 @@ root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <BrowserRouter>
+        <GlobalStyle />
         <App />
       </BrowserRouter>
     </ThemeProvider>
