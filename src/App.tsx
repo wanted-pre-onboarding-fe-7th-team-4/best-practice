aa94cded2bc828d3a73a8fcbc36848aa43912c52
movie42 <movie3342@gmail.com>
2022-10-28 01:56:37 +0900

    App.tsx 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useContext, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+
 import useLocalStorage from "@/lib/hooks/useLocalStorage";
 import { AuthContext } from "@/lib/states/ContextProvider";
 import Router from "./Routes/Router";
@@ -7,15 +7,11 @@ import Router from "./Routes/Router";
 function App() {
   const { setAuth } = useContext(AuthContext);
   const { getLocalStorage } = useLocalStorage();
-  const navigate = useNavigate();
 
   useEffect(() => {
     const token = getLocalStorage("access_token") as string;
     if (token) {
       setAuth({ authToken: token, isLogin: true });
-      navigate("/todo", { replace: true });
-    } else {
-      navigate("/", { replace: true });
     }
   }, []);
 
